b9751d64ad58937e11b4caf0063d395c4abdd0da
Eoin Lavery <dev@tenantc.co.uk>
2019-08-23 05:03:46 +0100

    Add folder structure for Login/Sign Up UI and User Model Struct

---
+++ My Top Nine/My Top Nine/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  My Top Nine
+//
+//  Created by Eoin Lavery on 23/08/2019.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+    var fullName: String
+    var country: String
+    var dateOfBirth: Date
+    var emailAddress: String
+    var occupation: String
+    var profileBio: String
+    var profilePhoto: UIImage?
+}
